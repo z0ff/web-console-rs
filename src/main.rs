08d0216c39a3163473f68a67ab6375a15f3f7c96
z0ff <ex1999@msn.com>
2021-06-28 15:39:46 +0900

    add index

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
 use std::process::Command;
-use once_cell::sync::OnceCell;
+//use once_cell::sync::OnceCell;
 //use std::lazy::OnceCell;
 use actix_web::{get, web, App, HttpRequest, HttpServer, Responder};
 //use sailfish::TemplateOnce;
+use sys_info::*;
 
-static outstr: OnceCell<String> = OnceCell::new();
+//static outstr: OnceCell<String> = OnceCell::new();
 
-#[get("/{cmd}/index.html")]
-async fn index(web::Path(cmd): web::Path<String>) -> impl Responder{
+#[get("/sh/{cmd}")]
+async fn sh(web::Path(cmd): web::Path<String>) -> impl Responder{
     let output = Command::new("bash").arg("-c").arg(&cmd).output().expect("Failed to excecute command");
     //outstr.set(format!("$ {}\n{}", &cmd, String::from_utf8_lossy(&output.stdout))).unwrap();
     //format!("{}", outstr.get().unwrap())
     format!("$ {}\n{}", &cmd, String::from_utf8_lossy(&output.stdout))
 }
 
+#[get("/")]
+async fn index() -> impl Responder{
+    format!("os: {} {}", os_type().unwrap(), os_release().unwrap())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(sh).service(index))
     .bind("127.0.0.1:8080")?
     .run()
     .await
