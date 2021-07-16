94857074e78d4853573d8924780860d4c0f4728c
z0ff <ex1999@msn.com>
2021-07-16 17:55:34 +0900

    add openssl

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use actix::prelude::*;
 use actix_files::NamedFile;
 use actix_web::{get, web, App, HttpRequest, HttpServer, Responder, Result};
 use actix_web_actors::ws;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 use sys_info::*;
 
 //static outstr: OnceCell<String> = OnceCell::new();
@@ -31,13 +32,22 @@ async fn info() -> impl Responder{
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // load ssl keys
+    // to create a self-signed temporary cert for testing:
+    // `openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'`
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
+
     HttpServer::new(|| {
         App::new()
             .service(index)
             .service(sh)
             .service(info)
     })
-    .bind("127.0.0.1:8080")?
+    .bind_openssl("127.0.0.1:8080", builder)?
     .run()
     .await
 }
