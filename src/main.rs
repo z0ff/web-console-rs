f8e0ed084213cf840a49cb2728b74c27fc8f3b95
z0ff <ex1999@msn.com>
2021-09-16 09:19:44 +0900

    add struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::time::{Duration, Instant};
 use std::process::Command;
+use std::time::{Duration, Instant};
 //use once_cell::sync::OnceCell;
 //use std::lazy::OnceCell;
 use actix::prelude::*;
@@ -7,10 +7,29 @@ use actix_files::NamedFile;
 use actix_web::{get, web, App, HttpRequest, HttpServer, Responder, Result};
 use actix_web_actors::ws;
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use serde::Deserialize;
 use sys_info::*;
 
 //static outstr: OnceCell<String> = OnceCell::new();
 
+#[derive(Deserialize)]
+struct Status {
+    os_type: String,
+    os_release: String,
+    cpu_num: u32,
+    cpu_speed: u64,
+    proc_total: u64,
+    load_one: f64,
+    load_five: f64,
+    load_fifteen: f64,
+    mem_total: u64,
+    mem_free: u64,
+    mem_avail: u64,
+    mem_buffers: u64,
+    mem_cached: u64,
+    mem_swap_total: u64,
+    mem_swap_free: u64,
+}
 
 #[get("/")]
 async fn index() -> Result<NamedFile> {
@@ -18,15 +37,19 @@ async fn index() -> Result<NamedFile> {
 }
 
 #[get("/sh/{cmd}")]
-async fn sh(web::Path(cmd): web::Path<String>) -> impl Responder{
-    let output = Command::new("bash").arg("-c").arg(&cmd).output().expect("Failed to excecute command");
+async fn sh(web::Path(cmd): web::Path<String>) -> impl Responder {
+    let output = Command::new("bash")
+        .arg("-c")
+        .arg(&cmd)
+        .output()
+        .expect("Failed to excecute command");
     //outstr.set(format!("$ {}\n{}", &cmd, String::from_utf8_lossy(&output.stdout))).unwrap();
     //format!("{}", outstr.get().unwrap())
     format!("$ {}\n{}", &cmd, String::from_utf8_lossy(&output.stdout))
 }
 
-#[get("/info")]
-async fn info() -> impl Responder{
+#[get("/status")]
+async fn status() -> impl Responder {
     format!("os: {} {}", os_type().unwrap(), os_release().unwrap())
 }
 
@@ -41,13 +64,8 @@ async fn main() -> std::io::Result<()> {
         .unwrap();
     builder.set_certificate_chain_file("cert.pem").unwrap();
 
-    HttpServer::new(|| {
-        App::new()
-            .service(index)
-            .service(sh)
-            .service(info)
-    })
-    .bind_openssl("127.0.0.1:8080", builder)?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(index).service(sh).service(status))
+        .bind_openssl("127.0.0.1:8080", builder)?
+        .run()
+        .await
 }
