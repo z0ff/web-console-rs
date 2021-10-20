3635de6a3393b365d90b431ee81aed429ed44ed3
z0ff <ex1999@msn.com>
2021-10-20 18:31:58 +0900

    improved StreamHandler and ScriptExecutor to work (incompletely)

--- src/script.rs
+++ src/script.rs
@@ -1,14 +1,16 @@
 use actix::prelude::*;
+use actix::AsyncContext;
 use actix_files::NamedFile;
 use actix_web::{
     get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder, Result,
 };
 use actix_web_actors::ws;
-use futures::prelude::*;
+//use futures::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::process::Stdio;
 use std::time::{Duration, Instant};
 use tokio::process::Command;
+use tokio::stream::{StreamExt};
 use tokio_util::codec::{FramedRead, LinesCodec};
 
 const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
@@ -24,7 +26,23 @@ struct MyWS {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Script {
-    lines: Vec<String>,
+    lines: String,
+}
+
+#[derive(Debug)]
+struct OutLn(String);
+
+impl StreamHandler<Result<OutLn, ws::ProtocolError>> for MyWS {
+    fn handle(
+        &mut self,
+        msg: Result<OutLn, ws::ProtocolError>,
+        ctx: &mut Self::Context,
+    ) {
+        match msg {
+            Ok(OutLn(line)) => ctx.text(line),
+            _ => ()
+        }
+    }
 }
 
 impl Actor for MyWS {
@@ -51,6 +69,7 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWS {
                 //#[must_use = "futures do nothing unless you `.await` or poll them"]
                 //exec_script(text, ctx);
                 //ctx.text(text);
+                ctx.notify(ScriptExecutor(text.to_string()));
             }
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
             Ok(ws::Message::Close(reason)) => {
@@ -66,19 +85,28 @@ impl Handler<ScriptExecutor> for MyWS {
     type Result = Result<(), ()>;
     fn handle(&mut self, msg: ScriptExecutor, ctx: &mut Self::Context) -> Self::Result {
         let script: Script = serde_json::from_str(msg.0.trim()).unwrap();
-        for ln in &script.lines {
-            let mut cmd = Command::new("bash").arg("-c").arg(ln);
-            cmd.stdout(Stdio::piped());
-            let mut child = cmd.spawn().expect("err");
+        let mut child = Command::new("bash")
+            .arg("-c")
+            .arg(&script.lines)
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("Failde");
+        //let mut cmd = Command::new(script.lines);
+        //cmd.stdout(Stdio::piped());
+        //let mut child = cmd.spawn().expect("err");
 
-            let stdout = child.stdout.take().unwrap();
+        let stdout = child.stdout.take().unwrap();
 
-            let mut reader = FramedRead::new(stdout, LinesCodec::new());
+        let reader = FramedRead::new(stdout, LinesCodec::new());
+        //let reader = BufReader::new(stdout).lines();
 
-            let fut = async move {
-                let status = child.spawn().await.expect("err");
-            };
-        }
+        let fut = async move {
+            let status = child.await.expect("err");
+            println!("child status was: {}", status);
+        };
+        let fut = actix::fut::wrap_future::<_, Self>(fut);
+        ctx.spawn(fut);
+        ctx.add_stream(reader.map(|l| Ok(OutLn(l.expect("Not a line")))));
         Ok(())
     }
 }
@@ -108,25 +136,4 @@ pub async fn script_index() -> Result<NamedFile> {
 pub async fn script_start(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
     let res = ws::start(MyWS::new(), &req, stream);
     res
-}
-
-async fn exec_script(script_str: String, ctx: &mut <MyWS as Actor>::Context) {
-    let script: Script = serde_json::from_str(script_str.trim()).unwrap();
-    for ln in &script.lines {
-        let mut child = Command::new("bash")
-            .arg("-c")
-            .arg(ln)
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("Failed");
-
-        let stdout = child.stdout.take().unwrap();
-
-        let mut reader = FramedRead::new(stdout, LinesCodec::new());
-
-        while let Some(line) = reader.next().await {
-            //println!("{}", line.unwrap());
-            ctx.text(line.unwrap());
-        }
-    }
 }
